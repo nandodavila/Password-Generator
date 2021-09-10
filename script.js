0767aafed22b2a8b7ae308a0f93ef48756d464dd
Nando <nandodavila94@gmail.com>
2021-09-09 20:58:39 -0400

    I might be getting somewhere kinda

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var characters = prompt("How may characters? (8-128)");
+    if (characters != "8") {alert("Incorrect Value!!");
+  }
+  var type = prompt("Upper Case or lower case?"); 
+    if (type != null) {confirm("Numbers and special characters?")};
+  }  
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
