8f6b32cc8c461ed944f29213e3e3d4193c0ba17e
Nando <nandodavila94@gmail.com>
2021-09-10 16:55:24 -0400

    added characters to be used

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var randomUpper = upperAlpha[Math.floor(Math.random() * upperAlpha.length)]
+var lowerAlpha = "abcdefghijklmnopqrstuvwxyz"
+var randomLower = lowerAlpha[Math.floor(Math.random() * upperAlpha.length)]
+var num = "1234567890"
+var randomNum = num[Math.floor(Math.random() * num.length)]
+var symbols ="!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var randomSymbol = symbols[Math.floor(Math.random() * symbols.length)]
+
+var character = [randomUpper, randomLower, randomNum, randomSymbol]
+var randomCharacter = character[Math.floor(Math.random() * character.length)]
+//var numCharacters = [Range(8, 128)]  
+
 function generatePassword() {
-  var characters = prompt("How may characters? (8-128)");
-    if (characters != "8") {alert("Incorrect Value!!");
+  var length = prompt("How may characters? (8-128)");
+   // if (length != 8, 128) {alert("Incorrect Value!!");
   }
-  var type = prompt("Upper Case or lower case?"); 
-    if (type != null) {confirm("Numbers and special characters?")};
-  }  
+  // var type = prompt("Upper Case or lower case?"); 
+  //   if (type != null) {confirm("Numbers and special characters?")};
+  // }
 
 // Write password to the #password input
 function writePassword() {
@@ -19,3 +32,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+for (var i = 0; i < 7; i++) {
+console.log(randomCharacter)
+}
